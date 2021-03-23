28f5d101f1d3fbade204571dda37585ca270b539
Caroline Guimarães <caroline.guimaraes@me.com.br>
2021-03-23 11:50:53 -0300

    Disponibilizando acesso ao Cors

--- config/server.js
+++ config/server.js
@@ -1,10 +1,17 @@
 const express = require('express'); //Incluindo a dependencia express
 const port = 8080;
 const consign = require('consign');
-
+const cors = require('cors');
 
 
 const app = express(); //chamando a função express
+
+app.use((req, res, next)=> {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3002");
+    app.use(cors());
+    next();
+});
+
 app.use(express.json());
 
 app.listen(port,() => { //configurando API para iniciar na porta 8080
